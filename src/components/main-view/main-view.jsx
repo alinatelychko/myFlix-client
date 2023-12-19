e4637a2c32288e8a6e7376ec3f516557616a5f79
alinatelychko <adelin9999@gmail.com>
2023-12-19 16:09:07 +0100

    3.5 react forms

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,25 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view"
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    fetch("https://movieapicf-30767e813dee.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://movieapicf-30767e813dee.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.map((movie) => {
@@ -27,7 +38,22 @@ export const MainView = () => {
 
         setMovies(moviesFromApi);
       });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -41,6 +67,12 @@ export const MainView = () => {
 
   return (
     <div>
+      <button onClick={() => { 
+        setUser(null); 
+        setToken(null); 
+        localStorage.clear();
+        }}>Logout</button>
+
       {movies.map((movie) => (
         <MovieCard
           key={movie._id}
